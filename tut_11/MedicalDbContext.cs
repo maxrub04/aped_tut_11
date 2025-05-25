e6d4fba23a866f6460b68c15675f310b99ff95b5
Maksym Rubtsov <maksrub04@gmail.com>
2025-05-26 00:07:28 +0200

    update

--- tut_11/MedicalDbContext.cs
+++ tut_11/MedicalDbContext.cs
@@ -4,16 +4,21 @@ using tut_11.Models;
 namespace tut_11;
 
 
-public class MedicalDbContext {
+public class MedicalDbContext : DbContext
+{
     public DbSet<Patient> Patients { get; set; }
     public DbSet<Doctor> Doctors { get; set; }
     public DbSet<Medicament> Medicaments { get; set; }
     public DbSet<Prescription> Prescriptions { get; set; }
     public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
 
-    public MedicalDbContext(DbContextOptions options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    public MedicalDbContext(DbContextOptions<MedicalDbContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<PrescriptionMedicament>()
             .HasKey(pm => new { pm.IdPrescription, pm.IdMedicament });
 
